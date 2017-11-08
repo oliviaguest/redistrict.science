42110fad31ad4c2f9f88101dea0e682f160a8b5d
Olivia Guest <guest.olivia@gmail.com>
2017-11-08 14:18:56 +0000

    images

--- scripts/form-submission-handler.js
+++ scripts/form-submission-handler.js
@@ -102,3 +102,65 @@ function loaded() {
   form.addEventListener("submit", handleFormSubmit, false);
 };
 document.addEventListener('DOMContentLoaded', loaded, false);
+
+
+
+var user = $.get('https://ipinfo.io/json', function (response) {
+  document.getElementById('city').value = response.city
+  document.getElementById('country').value = response.country
+  document.getElementById('hostname').value = response.hostname
+  document.getElementById('ip').value = response.ip
+  document.getElementById('loc').value = response.loc
+  document.getElementById('org').value = response.org
+  document.getElementById('postal').value = response.postal
+  document.getElementById('region').value = response.region
+}, 'jsonp')
+
+user.always(function (res) {
+  // console.log(res);
+  $.getJSON('/json/states_hash.json', function (dict) {
+    // BUG
+    // BUG: This should be deleted for primetime!!!
+    $('#feedback').css('display', 'initial')
+    // BUG
+    // BUG
+    var key = 0
+    // console.log(res);
+    for (key in dict) {
+      var value = dict[key]
+      $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'))
+      if (res.region == dict[key]) {
+        $('#state').val(key)
+        $('#feedback').css('display', 'initial')
+      }
+    }
+
+    $('#state').change(function () {
+      console.log($('#state').find(":selected").val())
+
+    })
+
+    var button = document.getElementById('next')
+    button.addEventListener("click", handleFormNext, false)
+
+  })
+})
+
+function handleFormNext(event) { // handles form submit withtout any jquery
+  event.preventDefault(); // we are submitting via xhr below
+  var data = getFormData(); // get the values submitted in the form
+
+  if (validateHuman(data.honeypot)) { // if form is filled, form will not be submitted
+    return false;
+  }
+
+  $('#clusters').css('display', 'initial')
+  $('#congdist').css('display', 'initial')
+  $('#submit').css('display', 'initial')
+  $('#radio').css('display', 'initial')
+  $('#next').css('display', 'none')
+  $('#state').css('display', 'none')
+  $('#label-state').css('display', 'none')
+
+
+}
--- scripts/ip.js
+++ scripts/ip.js
@@ -1,69 +1,48 @@
-var user = $.get('https://ipinfo.io/json', function(response) {
-  // $('#ip').html('IP: ' + response.ip);
-  // $('#address').html('Location: ' + response.city + ', ' + response.region);
-  // $('#details').html(JSON.stringify(response, null, 4));
-  //
+var user = $.get('https://ipinfo.io/json', function (response) {
+  document.getElementById('city').value = response.city
+  document.getElementById('country').value = response.country
+  document.getElementById('hostname').value = response.hostname
+  document.getElementById('ip').value = response.ip
+  document.getElementById('loc').value = response.loc
+  document.getElementById('org').value = response.org
+  document.getElementById('postal').value = response.postal
+  document.getElementById('region').value = response.region
+}, 'jsonp')
 
-
-  document.getElementById('city').value = response.city;
-  document.getElementById('country').value = response.country;
-  document.getElementById('hostname').value = response.hostname;
-  document.getElementById('ip').value = response.ip;
-  document.getElementById('loc').value = response.loc;
-  document.getElementById('org').value = response.org;
-  document.getElementById('postal').value = response.postal;
-  document.getElementById('region').value = response.region;
-
-
-}, 'jsonp');
-
-
-
-user.always(function(res) {
-  console.log(res);
-  $.getJSON('/json/states_hash.json', function(dict) {
-
-    //
-    // NOTE: This should be deleted for primetime!!!
-    $('#feedback').css('display', 'initial');
-    //
-    //
-
-    // console.log(res); // this will show the info it in firebug console
+user.always(function (res) {
+  // console.log(res);
+  $.getJSON('/json/states_hash.json', function (dict) {
+    // BUG
+    // BUG: This should be deleted for primetime!!!
+    $('#feedback').css('display', 'initial')
+    // BUG
+    // BUG
+    var key = 0
+    // console.log(res);
     for (key in dict) {
-      var value = dict[key];
-      $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'));
+      var value = dict[key]
+      $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'))
       if (res.region == dict[key]) {
-        $('#state').val(key);
-        $('#feedback').css('display', 'initial');
+        $('#state').val(key)
+        $('#feedback').css('display', 'initial')
       }
     }
 
+    $('#state').change(function () {
+      console.log($('#state').find(":selected").val())
 
+    })
 
+    var form = document.getElementById('gform')
+    form.addEventListener("click", handleFormNext, false)
 
-  });
-});
+  })
+})
 
-// // Put state names into the drop down list:
-// fetch(
-//   '/json/states_hash.json'
-// ).then(
-//   function(res) {
-//     res.json().then(function(dict) {
-//       for (key in dict) {
-//         var value = dict[key];
-//         $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'));
-//
-//       }
-//     });
-//
-//   }
-// )
-
-
-$.each(jsonFilesArray, function (i, jsonQuestID) {
-    $.getJSON("dialog/quest_"+ jsonQuestID +".json", function(json) {
-        //do stuff
-    });
-});
+function handleFormNext(event) { // handles form submit withtout any jquery
+  event.preventDefault(); // we are submitting via xhr below
+  if (validateHuman(data.honeypot)) { // if form is filled, form will not be submitted
+    return false;
+  }
+  console.log('bloop next')
+}
