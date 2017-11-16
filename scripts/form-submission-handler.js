29d677a9e1e7482aa7af1b4563af67a93240227d
Olivia Guest <guest.olivia@gmail.com>
2017-11-16 11:34:39 +0000

    less ping more zing

--- scripts/form-submission-handler.js
+++ scripts/form-submission-handler.js
@@ -107,48 +107,16 @@ function loaded() {
 document.addEventListener('DOMContentLoaded', loaded, false);
 
 
-
-var user = $.get('https://ipinfo.io?token=f26b5ab20fec6f', function(response) {
-  document.getElementById('city').value = response.city
-  document.getElementById('country').value = response.country
-  document.getElementById('hostname').value = response.hostname
-  document.getElementById('ip').value = response.ip
-  document.getElementById('loc').value = response.loc
-  document.getElementById('org').value = response.org
-  document.getElementById('postal').value = response.postal
-  document.getElementById('region').value = response.region
-}, 'jsonp')
-
-user.always(function(res) {
+$.getJSON('/json/states_hash.json', function(dict) {
+  $('#feedback').css('display', 'initial')
+  var key = 0
   // console.log(res);
-  $.getJSON('/json/states_hash.json', function(dict) {
-    // BUG
-    // BUG: This should be deleted for primetime!!!
-    $('#feedback').css('display', 'initial')
-    // BUG
-    // BUG
-    var key = 0
-    // console.log(res);
-    for (key in dict) {
-      var value = dict[key]
-      $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'))
-      if (res.region == dict[key]) {
-        $('#state').val(key)
-        $('#feedback').css('display', 'initial')
-      }
-    }
+  for (key in dict) {
+    var value = dict[key]
+    $('#state').append($('<option/>').attr('value', key).text(dict[key] + ' (' + key + ')'))
 
-    $('#state').change(function() {
-      var selected = $('#state').find(":selected").val()
-      console.log(selected)
-
-      placeImage(selected)
-    })
-
-
-  })
+  }
 })
-
 
 var button = document.getElementById('next')
 button.addEventListener("click", handleFormNext, false)
@@ -180,6 +148,18 @@ function hideWarning(event) {
 
 function handleFormNext(event) { // handles form submit withtout any jquery
   event.preventDefault(); // we are submitting via xhr below
+
+  var user = $.get('https://ipinfo.io?token=f26b5ab20fec6f', function(response) {
+    document.getElementById('city').value = response.city
+    document.getElementById('country').value = response.country
+    document.getElementById('hostname').value = response.hostname
+    document.getElementById('ip').value = response.ip
+    document.getElementById('loc').value = response.loc
+    document.getElementById('org').value = response.org
+    document.getElementById('postal').value = response.postal
+    document.getElementById('region').value = response.region
+  }, 'jsonp')
+
   var data = getFormData(); // get the values submitted in the form
 
   if (validateHuman(data.honeypot)) { // if form is filled, form will not be submitted
